e49cb70ae39051f5bc0e2a6b50d9682bc646cf88
kapil <kapil.personal9711@gmail.com>
2024-06-19 23:14:26 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,6 @@ import "../styles/App.css";
 
 const App = () => {
   const [value, setValue] = React.useState("");
-  console.log(value);
-  let strArr = [];
-  let str = "";
-  if (!value.includes("\\n")) strArr.push(<h1>{value}</h1>);
-  else {
-    for (let i = 0; i < value.length; i++) {
-      const ch = value[i];
-      if (ch === "#") continue;
-      if (ch === "\\" && value[i + 1] === "n") {
-        strArr.push(<h1>{str}</h1>);
-        strArr.push(<h1></h1>);
-        str = "";
-        i++;
-      } else str += ch;
-    }
-  }
 
   return (
     <section className="app">
@@ -33,8 +17,8 @@ const App = () => {
       </div>
       <div className="preview">
         <h1>
-          {strArr}
-          {/* <pre>{value}</pre> */}
+          {/* {strArr} */}
+          <pre>Heading</pre>
         </h1>
         {value.length === 0 && <p className="loading">loading...</p>}
       </div>
