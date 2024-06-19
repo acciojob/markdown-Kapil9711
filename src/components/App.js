a25f0ae6efaf5b24afa30ed83006e2a1c4441100
kapil <kapil.personal9711@gmail.com>
2024-06-19 22:32:26 +0530

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,2 +1,26 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import React from "react";
+import "../styles/App.css";
 
+const App = () => {
+  const [value, setValue] = React.useState("");
+  console.log(value);
+  return (
+    <section className="app">
+      <div className="textarea">
+        <textarea
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className="textarea"
+          name=""
+          id=""
+        />
+      </div>
+      <div className="preview">
+        <pre>{value}</pre>
+        {value.length === 0 && <p className="loading">loading...</p>}
+      </div>
+    </section>
+  );
+};
+
+export default App;
